1e39eae3e56c556a0534ae43488b6b746a03a88b
AssoRBE <association.rbe@gmail.com>
2025-09-26 21:28:36 +0200

    Passage à HashRouter - Solution définitive pour les 404
    - Changement de BrowserRouter vers HashRouter
    - Les URLs auront un # mais plus de 404 lors des refreshs
    - Solution fiable qui fonctionne sans configuration serveur
    - Les redirections précédentes restent comme fallback

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import './index.css'
@@ -9,11 +9,11 @@ import { UserProvider } from './context/UserContext'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <BrowserRouter>
+      <HashRouter>
         <UserProvider>
           <App />
         </UserProvider>
-      </BrowserRouter>
+      </HashRouter>
     </ChakraProvider>
   </React.StrictMode>,
 )
