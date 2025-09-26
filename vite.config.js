44da918fad3b243fc77c0e9bb62204d269cc7cb1
AssoRBE <association.rbe@gmail.com>
2025-09-26 21:01:22 +0200

    Mise à jour

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true
+    historyApiFallback: {
+      rewrites: [
+        { from: /^\/(?!api).*/, to: '/index.html' }
+      ]
+    }
   }
 })
